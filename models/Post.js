1ff3b23c67fa34de57e3f246b468c58ec8ba5941
William Edward Roberts <114425878+lasttycoon2506@users.noreply.github.com>
2023-04-28 16:49:18 -0400

    model for posts

--- models/Post.js
+++ models/Post.js
@@ -1,59 +1,19 @@
 import mongoose from 'mongoose'
 
-mongoose.connect(
-    "mongodb://localhost:27017/music_db",
-    { useNewUrlParser: true }
-)
-
-const db = mongoose.connection
-
-db.once("open", () => {
-    console.log("Successfully connected to MongoDB using Mongoose!")
-})
-
-const findPostSchema = mongoose.Schema({
-    userId: { type: Number },
-    instrument: { type: String, required: true },
-    experience: { type: String, required: true },
-    genre: { type: String, required: true },
+const postSchema = mongoose.Schema({
+    postId: { type: String, required: true },
+    userId: { type: String, required: true },
+    type: { type: String, required: true },
+    instrument: String,
+    experience: String,
+    genres: [],
     availability: { type: String, required: true },
-    recording: { type: Number, required: true }
-})
+    recordingExperience: String,
+    description: String,
+    imagePaths: [],
+    profileLink: String
+});
 
-const sellPostSchema = mongoose.Schema({
-    userId: { type: Number },
-    purpose: { type: String, required: true },
-    photos: { type: Photo, required: true },
-    profileLink: { type: String, required: true },
-})
+const Post = mongoose.model('Post', postSchema);
 
-const FindPost = mongoose.model('FindPost', findPostSchema)
-const SellPost = mongoose.model('SellPost', sellPostSchema)
-
-const createFindPost = async (instrument, experience, genre, availability, recording) => {
-    const findPost = new FindPost({ instrument, experience, genre, availability, recording })
-    return findPost.save()
-}
-
-const createSellPost = async (purpose, photos, profileLink) => {
-    const sellPost = new SellPost({ purpose, photos, profileLink })
-    return sellPost.save()
-}
-
-const retrieveFindPosts = async (userId) => {
-    const findPosts = await FindPost.findById({userId: userId })
-    return findPosts
-}
-
-const retrieveSellPosts = async (userId) => {
-    const sellPosts = await SellPost.findById({userId: userId })
-    return sellPosts
-}
-
-const retrieveAllPosts = async () => {
-    // needs work - search how to combine
-    const allPosts = FindPost.find() + SellPost.find()
-    return allPosts
-}
-
-export { createSellPost, retrieveSellPosts, createFindPost, retrieveFindPosts, retrieveAllPosts } 
+export default Post;
