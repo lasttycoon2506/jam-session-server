6b3f2d7071b47608a90dece22cab499baa366890
Gabriel Valenzuela <mgvalenzu@gmail.com>
2023-04-30 11:38:56 -0700

    Baisc CRUD operations complete

--- controllers/users.js
+++ controllers/users.js
@@ -7,13 +7,11 @@ const getAllUsers = asyncHandler(async (req, res, next) => {
     res.send(allUsers);
 })
 
-
 const getUser = asyncHandler(async (req, res, next) => {
     const id = req.params.id;
     const user = await User.find({userId: id });
     res.send(user);
 })
-
 
 const createUser = asyncHandler(async (req, res, next) => {
 
@@ -26,13 +24,13 @@ const createUser = asyncHandler(async (req, res, next) => {
     const experience = req.body.experience;
 
     const user = await new User( {
-        email: email, 
-        password: password, 
+        email: email,
+        password: password,
         location: location,
-        instruments: instruments, 
-        genres: genres, 
-        availability: availability, 
-        experience: experience 
+        instruments: instruments,
+        genres: genres,
+        availability: availability,
+        experience: experience
     })
     user.userId = user._id.toString();
     user.save();
@@ -40,25 +38,17 @@ const createUser = asyncHandler(async (req, res, next) => {
     res.send(user);
 })
 
-
-/* TODO: Incomplete - need to finish */
 const updateUserById = asyncHandler(async (req, res, next) => {
 
-    const userId = req.body.userId;
-    const email = req.body.email;
-    const password = req.body.password;
-    const location = req.body.location;
-    const instruments = req.body.instruments;
-    const genres = req.body.genres;
-    const availability = req.body.availability;
-    const experience = req.body.experience;
+    const paramId = req.params.id
+    const updatedFields = req.body
 
-    const user = await User.find({userId: userId})
-    user = await User.updateOne()
-
-    res.send(`NOT IMPLEMENTED: Update User ${req.params.id}`)
+    const updatedUser = await User.updateOne(
+        {userId: paramId},
+        {$set: updatedFields}
+    )
+    res.send(updatedUser)
 })
-
 
 const deleteUser = asyncHandler(async (req, res, next) => {
     const id = req.params.id;
@@ -68,69 +58,90 @@ const deleteUser = asyncHandler(async (req, res, next) => {
 
 // Mostly for wiping db of test users for now
 const deleteAllUsers = asyncHandler(async (req, res, next) => {
-    // const id = req.params.id;
-    const user = await User.deleteMany();
-    res.send(user);
-})
-
+  const user = await User.deleteMany();
+  res.send(user);
+});
 
 //Conventional versions
 
-// const getAllUsers = async(req, res) => {
-//     try{
-//         const users = await User.find();
-//         res.sendStatus(200).json(users);
+// const getAllUsers = async (req, res) => {
+//   try {
+//     const users = await User.find();
+//     res.status(200).json(users);
+//   } catch (err) {
+//     res.status(404);
+//   }
+// };
 
-//     } catch (err)  {
-//         res.status(404);
-//     }
-// }
+// const getUser = async (req, res) => {
+//   try {
+//     const users = await User.find({ userId: req.parmas.id });
+//     res.status(200).json(users);
+//   } catch (err) {
+//     res.status(404);
+//   }
+// };
 
+// const createUser = async (req, res) => {
+//   try {
+//     const email = req.body.email;
+//     const password = req.body.password;
+//     const location = req.body.location;
+//     const instruments = req.body.instruments;
+//     const genres = req.body.genres;
+//     const availability = req.body.availability;
+//     const experience = req.body.experience;
 
-// const getUser = async(req, res) => {
-//     try{
-//         const users = await User.find({ userId: req.parmas.id});
-//         res.sendStatus(200).json(users);
+//     const user = await new User({
+//       email: email,
+//       password: password,
+//       location: location,
+//       instruments: instruments,
+//       genres: genres,
+//       availability: availability,
+//       experience: experience,
+//     });
 
-//     } catch (err)  {
-//         res.status(404);
-//     }
-// }
+//     user.userId = user._id.toString();
+//     user.save();
 
+//     res.status(201).json(user);
+//   } catch (err) {
+//     res.status(400);
+//   }
+// };
 
-// const createUser = async(req, res) => {
-//     try{
-//         const users = await User.find();
-//         res.sendStatus(200).json(users);
+// const updateUserById = async (req, res) => {
+//   try {
+//     const paramId = req.params.id;
+//     const updatedFields = req.body;
 
-//     } catch (err)  {
-//         res.status(404);
-//     }
-// }
+//     const updatedUser = await User.updateOne(
+//       { userId: paramId },
+//       { $set: updatedFields }
+//     );
+//     res.status(200).json(updatedUser);
+//   } catch (err) {
+//     res.status(404);
+//   }
+// };
 
+// const deleteUser = async (req, res) => {
+//   try {
+//     const id = req.params.id;
+//     const user = await User.deleteOne({ userId: id });
+//     res.status(200).json(user);
+//   } catch (err) {
+//     res.status(404);
+//   }
+// };
 
-// const updateUserById = async(req, res) => {
-//     try{
-//         const users = await User.find();
-//         res.sendStatus(200).json(users);
-
-//     } catch (err)  {
-//         res.status(404);
-//     }
-// }
-
-
-// const deleteUser = async(req, res) => {
-//     try{
-//         const id = req.params.id;
-//         const user = await User.deleteOne({ userId: id});
-//         res.sendStatus(200).json(user);
-
-//     } catch (err)  {
-//         res.status(404);
-//     }
-// }
-
-
-export default getAllUsers 
-export {getAllUsers, getUser, createUser, updateUserById, deleteUser, deleteAllUsers}
+export default getAllUsers;
+export {
+  getAllUsers,
+  getUser,
+  createUser,
+  updateUserById,
+  deleteUser,
+  deleteAllUsers,
+};
