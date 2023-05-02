fb596f39ab0e0aad96f770283b09c9ee7ca27d4c
Gabriel Valenzuela <mgvalenzu@gmail.com>
2023-05-01 20:35:16 -0700

    Add missing 'name' field

--- models/User.js
+++ models/User.js
@@ -14,6 +14,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    name: {
+        type: String,
+        required: true
+    },
     location: {
         type: String,
         required: true
