287dc5d3f0dd75b2f2befa98c59c2df9a050f3c8
Jaron-S <jaronschoor@gmail.com>
2023-05-16 10:39:57 -0600

    temporarily removed file support

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 /* Routes with Files */
-app.post("/posts", upload.single("picture"), createPost);
+// app.post("/posts", upload.single("picture"), createPost);
 
 /* Routes */
 app.use("/users", usersRoutes);
