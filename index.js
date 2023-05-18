d90096eb4ebe80d5b0acbd51c308a68a3a35a358
Jaron-S <jaronschoor@gmail.com>
2023-05-18 11:29:15 -0600

    finalizing cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import multer from "multer";
 import cors from "cors";
+import helmet from "helmet";
 import usersRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import router from "./routes/auth.js";
@@ -11,11 +12,9 @@ import router from "./routes/auth.js";
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(cors({}));
 
 /* File Storage */
 const storage = multer.diskStorage({
