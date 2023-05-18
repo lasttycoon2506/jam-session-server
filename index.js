ed9278b5006e5cb279f2c1ad77f063efb1a01444
Jaron-S <jaronschoor@gmail.com>
2023-05-18 10:43:05 -0600

    modified cors

--- index.js
+++ index.js
@@ -14,7 +14,12 @@ const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    exposedHeaders: "Access-Control-Allow-Origin",
+  })
+);
 
 /* File Storage */
 const storage = multer.diskStorage({
