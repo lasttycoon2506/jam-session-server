88cb26f4b13d8d1b1f773209e00721e7f50dafef
Jaron-S <jaronschoor@gmail.com>
2023-05-18 09:09:57 -0600

    specified cors headers

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ import router from "./routes/auth.js";
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    exposedHeaders: "Access-Control-Allow-Origin",
+  })
+);
 
 /* File Storage */
 const storage = multer.diskStorage({
